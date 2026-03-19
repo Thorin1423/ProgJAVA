c31b6e9038d483c10734ea3822f9c926eb383298
wiktorw <wiktorw@mat.umk.pl>
2026-03-19 22:55:16 +0100

    dodanie MonteCarlo i Figura

--- src/Figura.java
+++ src/Figura.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class Figura {
+    private double promien;
+
+    //konstruktor - ustala promien figury
+    public Figura(double promien) {
+        this.promien = promien;
+    }
+
+    //sprawdzenie czy miesci sie w kole
+    public boolean czyPunktJestWewnatrz(double x, double y) {
+        return (x * x) + (y * y) <= (promien * promien);
+    }
 }
+
